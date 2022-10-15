bf80b64a5fd05c25affa8c91a1c6530761ef695a
varanustoken <varanustoken@gmail.com>
2022-10-15 22:56:00 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,8 @@
                         <h1 class="best-template animated" data-animation="fadeInUpShorter" data-animation-delay="1.5s">PRIME APE CHAIN</h1>
                         <h3 class="d-block white animated" data-animation="fadeInUpShorter" data-animation-delay="1.6s">The Next Gen Multi Proof Of Stake Blockchain With Fast And Scalable</h3>
                         <div class="mt-5">
-                            <a href="https://exchange.pancakeswap.finance/#/swap?inputCurrency=0xf41d441981Ea9f641e3DccDf6E40C225dB65F147" target="_blank" class="btn btn-lg btn-gradient-blue btn-glow mr-4 mb-2 animated" data-animation="fadeInUpShorter" data-animation-delay="1.7s">Buy on PancakeSwap</a>
-                            <a href="https://bscscan.com/token/0xf41d441981Ea9f641e3DccDf6E40C225dB65F147" class="btn btn-lg btn-gradient-blue btn-glow mb-2 animated" target="_blank" data-animation="fadeInUpShorter" data-animation-delay="1.8s">BscScan Info</a>
+                            <a href="https://exchange.pancakeswap.finance/#/swap?inputCurrency=0x1A9365Dc069F929e6e42ca7506E68464DA7424A2" target="_blank" class="btn btn-lg btn-gradient-blue btn-glow mr-4 mb-2 animated" data-animation="fadeInUpShorter" data-animation-delay="1.7s">Buy on PancakeSwap</a>
+                            <a href="https://bscscan.com/token/0x1A9365Dc069F929e6e42ca7506E68464DA7424A2" class="btn btn-lg btn-gradient-blue btn-glow mb-2 animated" target="_blank" data-animation="fadeInUpShorter" data-animation-delay="1.8s">BscScan Info</a>
                         </div>
                     </div>
                 </div>
@@ -110,7 +110,7 @@
 
                       <div style="-webkit-box-shadow: 0px 28px 28px -24px rgb(56 62 86 / 25%);box-shadow: 0px 28px 28px -24px rgb(56 62 86 / 25%);border-radius: 12px;padding: 16px 20px;text-decoration: none !important;display: block;">
                           <div class="text-center">
-                            <input type="text" value="0xf41d441981Ea9f641e3DccDf6E40C225dB65F147" id="smartcontractaddress" readonly style="width:100%;text-align:center;color:#ffffff;background:none;border:none" />
+                            <input type="text" value="0x1A9365Dc069F929e6e42ca7506E68464DA7424A2" id="smartcontractaddress" readonly style="width:100%;text-align:center;color:#ffffff;background:none;border:none" />
                           </div>
                           <button type="button" style="cursor: pointer;border-radius:15px;width:100%" class="btn btn-primary" onclick="copyContract()">
                               <span class="fa fa-copy"></span>
